271818095cf11f65940f54c9328e675988986c55
mr.Kuchkarov <yekut1515@yandex.ru>
2023-06-10 21:42:56 +0500

    Fixed errors from the reviewer

--- src/components/utils.js
+++ src/components/utils.js
@@ -11,28 +11,26 @@ export function setLoading(isLoading, button, buttonText='Сохранить', l
     }
 }
 
-// можно сделать универсальную функцию, которая принимает функцию запроса, объект события и текст во время загрузки
+
 export function handleSubmit(request, evt, loadingText = "Сохранение...") {
-    // всегда нужно предотвращать перезагрузку формы при сабмите
+ 
      evt.preventDefault();
-   
-     // универсально получаем кнопку сабмита из `evt`
+
      const submitButton = evt.submitter;
-     // записываем начальный текст кнопки до вызова запроса
+
      const initialText = submitButton.textContent;
-     // изменяем текст кнопки до вызова запроса
+
      setLoading(true, submitButton, initialText, loadingText);
      request()
        .then(() => {
-         // любую форму нужно очищать после успешного ответа от сервера
-         // а также `reset` может запустить деактивацию кнопки сабмита (смотрите в `validate.js`)
+
          evt.target.reset();
        })
        .catch((error) => {
-         // в каждом запросе нужно ловить ошибку
+
          console.error(`Ошибка: ${error}`);
        })
-       // в каждом запросе в `finally` нужно возвращать обратно начальный текст кнопки
+
        .finally(() => {
         setLoading(false, submitButton, initialText);
        });
